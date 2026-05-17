e9b92ecce012aacae6f55bcccdb0113bc35e8678
narenhft <narenc@outlook.com>
2026-05-17 20:13:13 +0530

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-82JZ92Z7KV"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-82JZ92Z7KV');
+        `}} />
+      </head>
       <body>
         <Navbar />
         <main>{children}</main>
